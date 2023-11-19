7c3c906cb51d8b30d7f896748754057b2408b0d6
MarioLour <151404480+MarioLour@users.noreply.github.com>
2023-11-19 16:05:11 -0300

    Update build.sh

--- blog/build.sh
+++ blog/build.sh
@@ -2,6 +2,11 @@
 # exit on error
 set -o errexit
 
+# Acessa o diretório onde estão os arquivos
+cd "$(dirname "$0")"
+
+# Instala os requisitos
 pip install -r requirements.txt
 
+# Executa o comando para coletar arquivos estáticos
 python manage.py collectstatic --no-input
